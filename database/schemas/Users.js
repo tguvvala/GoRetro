1f39c6b4f4fc1ea1b25c0f60d1bf5f5b5b59a075
Patty Kovash <pattykovash@gmail.com>
2018-01-11 19:31:54 -0700

    Add user schema

---
+++ database/schemas/Users.js
@@ -0,0 +1,23 @@
+module.exports.userSchema = mongoose.Schema({
+  userName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  fbId: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  fbAccessToken: {
+    type: String,
+    unique: true,
+    required: true
+  }
+});
+
+module.exports.User = mongoose.model('User', usersSchema);
