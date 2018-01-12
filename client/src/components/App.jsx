ae73810940d398c19afbd96cebbd821b21b7ad2f
Patty Kovash <pattykovash@gmail.com>
2018-01-12 02:22:27 -0700

    Attempt to download Facebook SDK

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,10 +4,51 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home.jsx';
 import NewListing from './NewListing.jsx';
 import ViewListing from './ViewListing.jsx';
+import FacebookConfig from '../../../facebookConfig';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    window.FB.getLoginStatus(function(response) {
+      if (response.status === 'connected') {
+        console.log('Logged in.');
+      } else {
+        window.FB.login();
+      }
+    });
+
+    window.fbAsyncInit = function() {
+      window.FB.init({
+        appId: FacebookConfig.clientID,
+        cookie: true,
+        xfbml: true,
+        version: 'v2.1'
+      });
+
+      window.FB.Event.subscribe('auth.statusChange', (response) => {
+        console.log('RESPONSE: ', response);
+        if (response.authResponse) {
+          this.updateLoggedInState(Response);
+        } else {
+          this.updateLoggedOutState();
+        }
+      });
+    }.bind(this);
+
+    ((d, s, id) => {
+      let js;
+      let fjs = d.getElementsByTagName(s)[0];
+      if (d.getelementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = '//connect/facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }, (document, 'script', 'facebook-jssdk'));
   }
 
   render() {
