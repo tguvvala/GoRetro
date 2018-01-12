79dc41c66111e3b54b9545e48ae50c97ce021fe2
Grant Spilsbury <grantspilsbury@gmail.com>
2018-01-13 09:31:33 +1100

    Ajax calls

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -13,23 +13,38 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.getListings = this.getListings.bind(this);
+    this.handleCategoryClick = this.handleCategoryClick.bind(this);
     this.state = {
-      listings: []
+      listings: [],
+      category: ''
     };
   }
 
+  // poll server every few seconds
+
   componentDidMount() {
+    this.getListings();
+    setInterval(() => {
+      this.getListings
+    }, 1000)
+  }
+
+  handleCategoryClick(category) {
+    category = category || '';
+    this.setState({ category: category });
     this.getListings();
   }
 
-  getListings(category) {
+  getListings() {
+    // add this.state.category to params
     $.ajax({
       url: '/listings',
       success: (listings) => {
         this.setState({
           listings: listings
         })
-        console.log('---------------Listings data', listings)
+        console.log('---------------Listings', listings)
+
       },
       error: (err) => {
         console.log('Get listings error', err);
@@ -40,12 +55,12 @@ class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/sign-up' component={SignUp}/>
-        <Route exact path='/sign-in' component={SignIn}/>
-        <RouteProps path='/new-listing' component={NewListing} someProps={'Stringy String'} moreProps={5}/> 
-        <RouteProps path='/user-listings' component={UserListings} someProps={'Stringy String'} moreProps={5}/> 
-        <Route path='/view-listing' component={ViewListing}/>
+        <Route exact path='/' component={ Home }/>
+        <Route exact path='/sign-up' component={ SignUp }/>
+        <Route exact path='/sign-in' component={ SignIn }/>
+        <RouteProps path='/new-listing' component={ NewListing } moreProps={5}/> 
+        <RouteProps path='/user-listings' component={ UserListings } someProps={'Stringy String'} moreProps={ 5 }/> 
+        <Route path='/view-listing' component={ ViewListing }/>
       </Switch>
     )
   }
--- client/src/components/NewListing.jsx
+++ client/src/components/NewListing.jsx
@@ -1,62 +1,188 @@
 import React from 'react';
+import $ from 'jquery';
 import { Link } from 'react-router-dom';
 
-const NewListing = (props) => (
-  <div>
-    <div className="container">
-      <ol className="breadcrumb">
-        <li className="breadcrumb-item active"><Link to="/">Home</Link></li>
-      </ol>
-      <form>
-        <img src="imagePlaceholder.png" className="img-photo img-thumbnail rounded" alt="Photo" />
-        <div className="form-group">
-          <input type="file" className="form-control-file" id="selectImage" />
-        </div>
+class NewListing extends React.Component {
 
-        <div className="form-group">
-          <input type="text" className="form-control form-control-lg" id="titleInput" placeholder="Lego Title" />
-        </div>
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.handleSubmitClick = this.handleSubmitClick.bind(this);
+    this.postListing = this.postListing.bind(this);
+    this.clearFields = this.clearFields.bind(this);
 
-        <div className="form-group">
-          <textarea className="form-control form-control-lg" id="descriptionInput" rows="3"  placeholder="Description"></textarea>
-        </div>
+    this.state = {
+      listing: {
+        title: '',
+        description: '',
+        category: '',
+        name: '',
+        email: '',
+        zipCode: '',
+        condition: '',
+        legoSetCode: ''
+      }
+    };
+  }
 
-        <div className="form-group">
-          <select className="form-control form-control-lg" id="exampleFormControlSelect1">
-            <option>-- CATEGORY --</option>
-            <option>Star Wars</option>
-            <option>Cityscapes</option>
-            <option>Farmers</option>
-          </select>
-        </div>
+  handleSubmitClick(e) {
+    e.preventDefault();
+    this.postListing();
+  }
 
-        <div className="form-group">
-          <input type="text" className="form-control form-control-lg" id="nameInput" placeholder="Your Name" />
-        </div>
+  handleNameInputChange(event) {
+    this.setState({
+      listing: {
+        name: event.target.value
+      }
+    });
+  }
 
-        <div className="form-group">
-          <input type="email" className="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" />
-        </div>
+  clearFields() {
+    console.log('Clearing fields')
+    this.setState({
+      listing: {
+        title: '',
+        description: '',
+        category: '',
+        name: '',
+        email: '',
+        zipCode: '',
+        condition: '',
+        legoSetCode: ''
+      }
+    })
+  }
 
-        <div className="form-group">
-          <input type="text" className="form-control form-control-lg" id="zipInput" placeholder="ZIP Code" />
-        </div>
+  postListing() {
+    $.post({
+      url: '/listing',
+      dataType: 'json',
+      data: this.state.listing,
+      success: () => {
+        this.clearFields();
+      },
+      error: (err) => {
+        console.log('Post listing error', err);
+      }
+    });
+  }
 
-        <div className="form-group row">
-          <div className="col-sm-10">
-            <button type="submit" className="btn btn-primary">Submit</button>
-            <Link to="/">
-              <button className="btn">
-                Cancel
-               </button>
-            </Link>
-          </div>
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item active"><Link to="/">Home</Link></li>
+          </ol>
+          <form>
+            <img src="imagePlaceholder.png" className="img-photo img-thumbnail rounded" alt="Photo" />
+            <div className="form-group">
+              <input type="file" className="form-control-file" id="selectImage"/>
+            </div>
+
+            <div className="form-group">
+              <input type="text" className="form-control form-control-lg" id="titleInput" placeholder="Title" />
+              <small className="form-text text-muted">The title of the lego set.</small>
+            </div>
+
+            <div className="form-group">
+              <textarea className="form-control form-control-lg" id="descriptionInput" rows="3"  placeholder="Description"></textarea>
+            </div>
+
+            <div className="form-group">
+              <select className="form-control form-control-lg" id="categorySelect">
+                <option>-- CATEGORY --</option>
+                <option>Star Wars</option>
+                <option>Cityscapes</option>
+                <option>Farmers</option>
+              </select>
+            </div>
+
+            <div className="form-group">
+              <select className="form-control form-control-lg" id="conditionSelect">
+                <option>-- CONDITION --</option>
+                <option>New</option>
+                <option>Used</option>
+                <option>Missing Pieces</option>
+              </select>
+            </div>
+
+            <div className="form-group">
+              <input type="text" className="form-control form-control-lg" id="legoSetCodeInput" placeholder="Code" />
+            </div>
+            <small className="form-text text-muted">The code of the lego set.</small>
+
+            <div className="form-group">
+              <input type="text" className="form-control form-control-lg" id="nameInput" placeholder="Your Name" onChange={this.handleNameInputChange.bind(this)} value={this.state.listing.name} />
+            </div>
+
+            <div className="form-group">
+              <input type="email" className="form-control form-control-lg" id="emailInput" aria-describedby="emailHelp" placeholder="Email" />
+            </div>
+
+            <div className="form-group">
+              <input type="text" className="form-control form-control-lg" id="zipInput" placeholder="ZIP Code" />
+            </div>
+
+            <div className="form-group row">
+              <div className="col-sm-10">
+                <button className="btn btn-primary" onClick={ this.handleSubmitClick }>Submit</button>
+                <Link to="/">
+                  <button className="btn">
+                    Cancel
+                   </button>
+                </Link>
+              </div>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
-    <Link to="/user-listings">user listings</Link>
-    <Link to="/sign-up">sign-up</Link>
-  </div>
-);
+        <Link to="/user-listings">user listings</Link>
+        <Link to="/sign-up">sign-up</Link>
+      </div>
+    );
+  }
+}
+
 
 export default NewListing;
+
+
+    // var title = this.titleElement.value;
+
+
+    // this.titleElement = document.getElementById('titleInput');
+    // this.descriptionElement = document.getElementById('descriptionInput');
+    // console.log(this)
+    // var description = this.descriptionElement.value;
+    // console.log(description)
+    // this.categoryElement = document.getElementById('categorySelect');
+    // this.nameElement = document.getElementById('nameInput');
+    // this.emailElement = document.getElementById('emailInput');
+    // this.zipCodeElement = document.getElementById('zipInput');
+    // this.conditionSelectElement = document.getElementById('conditionSelect');
+    // this.legoSetCodeElement = document.getElementById('legoSetCodeInput');
+
+
+
+    // var description = this.descriptionElement.value;
+    // console.log(description)
+    // console.log(this.categoryElement);
+    // var category = this.categoryElement.options[categoryElement.selectedIndex].text;
+    // var name = this.nameElement.value;
+    // var email = this.emailElement.value;
+    // var zipCode = this.zipCodeElement.value;
+    // var condition = this.conditionSelectElement.options[conditionSelect.selectedIndex].text;
+    // var legoSetCode = this.legoSetCodeElement.value;
+
+    // var listing = {
+    //   userId: userId,
+    //   title: title,
+    //   description: description,
+    //   category: category,
+    //   name: name,
+    //   email: email,
+    //   zipCode: zipCode,
+    //   condition: condition,
+    //   legoSetCode: legoSetCode
+    // };
