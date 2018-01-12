e6a150ecdf93d849b7e22fd29d7a002f40f82d73
Patty Kovash <pattykovash@gmail.com>
2018-01-11 21:35:06 -0700

    Add passport authentication

--- server/index.js
+++ server/index.js
@@ -1,12 +1,51 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
 const db = require('../database/index');
 const app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+passport.use(new FacebookStrategy(
+  {
+    clientID: '186862941903694',
+    clientSecret: '55c5480cff3e9446d6240fb029445140',
+    callbackURL: 'http://localhost:8080/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'email', 'name']
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log('accessToken: ', accessToken);
+    console.log('refreshToken: ', refreshToken);
+    console.log('profile: ', profile);
+    console.log('done: ', done);
+    let userInfo = {
+      access: accessToken,
+      info: done
+    };
+
+    db.User.findOneAndUpdate(
+      { fbId: { $eq: done.id } },
+      {
+        userName: `${ done.name.givenName } ${ done.name.familyName }`,
+        email: done.emails[0].value,
+        fbId: done.id,
+        fbAccessToken: profile.access_token
+      },
+      {
+        upsert: true,
+        new: true,
+        runValidators: true
+      }, (err, user) => {
+        if (err) {
+          throw err;
+        } else {
+          return done(null, user);
+        }
+      });
+  }));
 
 app.get('/listings', (req, res) => {
 
