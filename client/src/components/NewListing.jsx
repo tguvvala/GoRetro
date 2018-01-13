308c5686c89b3c70040706cc5e9ee5d78fc625cf
Patty Kovash <pattykovash@gmail.com>
2018-01-12 20:02:51 -0700

    Refactor to use one handleChange method. Dropdowns mapped

--- client/src/components/NewListing.jsx
+++ client/src/components/NewListing.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import $ from 'jquery';
 import { Link, Redirect, Route } from 'react-router-dom';
+import { ValidatorComponent } from 'react-form-validator-core';
 
 class NewListing extends React.Component {
 
   constructor(props) {
     super(props);
-    this.props = props;
+
+    this.state = {
+      title: '',
+      description: '',
+      category: '',
+      username: '',
+      email: '',
+      zipCode: '',
+      condition: '',
+      legoSetCode: '',
+      imageUrl: ''
+    };
+
     this.handleSubmitClick = this.handleSubmitClick.bind(this);
     this.postListing = this.postListing.bind(this);
     this.clearFields = this.clearFields.bind(this);
-
-    this.state = {
-      listing: {
-        title: '',
-        description: '',
-        category: '',
-        username: '',
-        email: '',
-        zipCode: '',
-        condition: '',
-        legoSetCode: '',
-        imageUrl: ''
-      }
-    };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleSubmitClick(e) {
@@ -31,111 +31,36 @@ class NewListing extends React.Component {
     this.postListing(this.clearFields);
   }
 
-  handleTitleInputChange(event) {
+  handleChange (event) {
+    let name = event.nativeEvent.target.name;
+    let value = event.nativeEvent.target.value;
     this.setState({
-      listing: {
-        ...this.state.listing,
-        title: event.target.value
-      }
-    });
-  }
-
-  handleDescriptionInputChange(event) {
-    this.setState({
-      listing: {
-        ...this.state.listing,
-        description: event.target.value
-      }
-    });
-  }
-
-  handleCategoryInputChange(event) {
-    this.setState({
-      listing: {
-        ...this.state.listing,
-        category: event.target.value
-      }
-    });
-  }
-
-  handleNameInputChange(event) {
-    this.setState({
-      listing: {
-        ...this.state.listing,
-        username: event.target.value
-      }
-    });
-  }
-
-  handleEmailInputChange(event) {
-    this.setState({
-      listing: {
-        ...this.state.listing,
-        email: event.target.value
-      }
-    });
-  }
-
-  handleZipCodeInputChange(event) {
-    this.setState({
-      listing: {
-        ...this.state.listing,
-        zipCode: event.target.value
-      }
-    });
-  }
-
-  handleConditionInputChange(event) {
-    this.setState({
-      listing: {
-        ...this.state.listing,
-        condition: event.target.value
-      }
-    });
-  }
-
-  handleLegoSetCodeInputChange(event) {
-    this.setState({
-      listing: {
-        ...this.state.listing,
-        legoSetCode: event.target.value
-      }
-    });
-  }
-
-  handleImageUrlInputChange(event) {
-    this.setState({
-      listing: {
-        ...this.state.listing,
-        imageUrl: event.target.value
-      }
+      [name]: value
     });
   }
 
   clearFields() {
-    console.log('Clearing fields')
+    console.log('Clearing fields');
     this.setState({
-      listing: {
-        title: '',
-        description: '',
-        category: '',
-        username: '',
-        email: '',
-        zipCode: '',
-        condition: '',
-        legoSetCode: '',
-        imageUrl: ''
-      }
-    })
+      title: '',
+      description: '',
+      category: '',
+      username: '',
+      email: '',
+      zipCode: '',
+      condition: '',
+      legoSetCode: '',
+      imageUrl: ''
+    });
   }
 
   postListing(callback) {
     $.ajax({
       type: 'POST',
       url: '/listings',
-      data: this.state.listing,
+      data: this.state,
       success: function() {
-        console.log('Succes on react side')
+        console.log('Succes on react side');
         // figure out how to do this the React Router way
         window.location.href = '/';
       },
@@ -159,47 +84,47 @@ class NewListing extends React.Component {
             </div>
 
             <div className="form-group">
-              <input type="text" className="form-control form-control-lg" id="titleInput" placeholder="Title" onChange={this.handleTitleInputChange.bind(this)} value={this.state.listing.title}/>
+              <input type="text"className="form-control form-control-lg" id="titleInput" placeholder="Title" onChange={this.handleChange} name="title" value={this.state.title}/>
               <small className="form-text text-muted">The title of the lego set.</small>
             </div>
 
             <div className="form-group">
-              <textarea className="form-control form-control-lg" id="descriptionInput" rows="3"  placeholder="Description" onChange={this.handleDescriptionInputChange.bind(this)} value={this.state.listing.description}></textarea>
+              <textarea className="form-control form-control-lg" id="descriptionInput" rows="3"  placeholder="Description" onChange={this.handleChange} name="description" value={this.state.description}></textarea>
             </div>
 
             <div className="form-group">
-              <select className="form-control form-control-lg" id="categorySelect">
-                <option>-- CATEGORY --</option>
-                <option>Star Wars</option>
-                <option>Cityscapes</option>
-                <option>Farmers</option>
+              <select className="form-control form-control-lg" id="categorySelect" name="category" value={this.state.category} onChange={this.handleChange}>
+                <option value="">-- CATEGORY --</option>
+                <option value="Star Wars">Star Wars</option>
+                <option value="Cityscapes">Cityscapes</option>
+                <option value="Star Wars">Farmers</option>
               </select>
             </div>
 
             <div className="form-group">
-              <select className="form-control form-control-lg" id="conditionSelect">
-                <option>-- CONDITION --</option>
-                <option>New</option>
-                <option>Used</option>
-                <option>Missing Pieces</option>
+              <select className="form-control form-control-lg" id="conditionSelect" name="condition" value={this.state.condition} onChange={this.handleChange}>
+                <option value="">-- CONDITION --</option>
+                <option value="new">New</option>
+                <option value="used">Used</option>
+                <option value="missing pieces">Missing Pieces</option>
               </select>
             </div>
 
             <div className="form-group">
-              <input type="text" className="form-control form-control-lg" id="legoSetCodeInput" placeholder="Lego Set Code" onChange={this.handleLegoSetCodeInputChange.bind(this)} value={this.state.listing.legoSetCode}/>
+              <input type="text" className="form-control form-control-lg" id="legoSetCodeInput" placeholder="Lego Set Code" onChange={this.handleChange} name="legoSetCode" value={this.state.legoSetCode}/>
               <small className="form-text text-muted">The code of the lego set.</small>
             </div>
 
             <div className="form-group">
-              <input type="text" className="form-control form-control-lg" id="nameInput" placeholder="Your Name" onChange={this.handleNameInputChange.bind(this)} value={this.state.listing.username} />
+              <input type="text" className="form-control form-control-lg" id="nameInput" placeholder="Your Name" onChange={this.handleChange} name="username" value={this.state.username} />
             </div>
 
             <div className="form-group">
-              <input type="email" className="form-control form-control-lg" id="emailInput" aria-describedby="emailHelp" placeholder="Email" onChange={this.handleEmailInputChange.bind(this)} value={this.state.listing.email}  />
+              <input type="email" className="form-control form-control-lg" id="emailInput" aria-describedby="emailHelp" placeholder="Email" onChange={this.handleChange} name="email" value={this.state.email} />
             </div>
 
             <div className="form-group">
-              <input type="text" className="form-control form-control-lg" id="zipInput" placeholder="ZIP Code" onChange={this.handleZipCodeInputChange.bind(this)} value={this.state.listing.zipCode} />
+              <input type="text" className="form-control form-control-lg" id="zipInput" placeholder="ZIP Code" onChange={this.handleChange} name="zipCode" value={this.state.zipCode} />
             </div>
 
             <div className="form-group row">
@@ -208,7 +133,7 @@ class NewListing extends React.Component {
                 <Link to="/">
                   <button className="btn">
                     Cancel
-                   </button>
+                  </button>
                 </Link>
               </div>
             </div>
