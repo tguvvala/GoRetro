bc08e9db03ac030cf50e1d6ad2e9b2cb4ae0ae9f
Luna Kim <lunakim@Lunas-MacBook-Pro.local>
2018-01-12 16:36:07 -0800

    Footer added and Categories Changed

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,32 +1,50 @@
 import React from 'react';
 
-const Categories = (props) => (
-  <div>
-  <section>
-    <div className="container">
-      <div className="row">
-       <div className="col-sm-2 categories">
-         <img src="batman.png" className="category"/>
-       </div>
-       <div className="col-sm-2 categories">
-         <img src="city.png" className="category"/>
-       </div>
-       <div className="col-sm-2 categories">
-         <img src="ninjago.png" className="category"/>
-       </div>
-       <div className="col-sm-2 categories">
-         <img src="friends.png" className="category"/>
-       </div>
-       <div className="col-sm-2 categories">
-         <img src="starwars.png" className="category"/>
-       </div>
-       <div className="col-sm-2 categories">
-         <img src="HP.png" className="category"/>
-       </div>
-     </div>
-    </div>
-  </section>
-</div>
-)
+
+class Categories extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: ''
+    };
+  }
+
+
+  handleClick(e) {
+    this.setState({category: e.currentTarget.dataset.cat});
+  }
+
+  render() {
+    return (
+      <div>
+        <section>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-2 categories">
+                <button><img src="batman.png" className="category" data-cat="batman" onClick={this.handleClick.bind(this)}/></button>
+              </div>
+              <div className="col-sm-2 categories">
+                <img src="city.png" className="category" value="city" onClick={this.handleClick.bind(this)}/>
+              </div>
+              <div className="col-sm-2 categories">
+                <img src="ninjago.png" className="category" value="ninjago" onClick={this.handleClick.bind(this)}/>
+              </div>
+              <div className="col-sm-2 categories">
+                <img src="friends.png" className="category" value="friends" onClick={this.handleClick.bind(this)}/>
+              </div>
+              <div className="col-sm-2 categories">
+                <img src="starwars.png" className="category" value="starwars" onClick={this.handleClick.bind(this)}/>
+              </div>
+              <div className="col-sm-2 categories">
+                <img src="HP.png" className="category" value="harrypotter" onClick={this.handleClick.bind(this)}/>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+
+}
 
 export default Categories;
--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -1,88 +1,52 @@
 import React from 'react';
 import InfiniteGrid from 'react-infinite-grid';
 import { Link } from 'react-router-dom';
-var tempProps = {color: 'red' };
+import Listing from './Listing.jsx';
+
+var tempProps = [{
+  'title': 'Star Wars Boba Fett',
+  'description': 'Brand new. My kid wanted Batman.',
+  'condition': 'New',
+  'category': 'Star Wars',
+  'username': 'SuburbanDad',
+  'email': 'bob@bob.com',
+  'zipCode': '52645',
+  'legoSetCode': '4D1F56',
+  'imageUrl': 'http://aws.com/bucket/images/listing/1/3.jpg',
+  'id': '12345'
+},
+{
+  'title': 'Star Wars Boba Fett',
+  'description': 'Brand new. My kid wanted Batman.',
+  'condition': 'New',
+  'category': 'Star Wars',
+  'username': 'SuburbanDad',
+  'email': 'bob@bob.com',
+  'zipCode': '52645',
+  'legoSetCode': '4D1F56',
+  'imageUrl': 'http://aws.com/bucket/images/listing/1/3.jpg',
+  'id': '12545'
+},
+{
+  'title': 'Star Wars Boba Fett',
+  'description': 'Brand new. My kid wanted Batman.',
+  'condition': 'New',
+  'category': 'Star Wars',
+  'username': 'SuburbanDad',
+  'email': 'bob@bob.com',
+  'zipCode': '52645',
+  'legoSetCode': '4D1F56',
+  'imageUrl': 'http://aws.com/bucket/images/listing/1/3.jpg',
+  'id': '12685'
+}];
+
 const Listings = (props) => (
-  <div>
-    <div className="container">
-
-      <div className="row">
-        <div className="col">
-          <Link to='/view-listing'>
-            <div className="card" >
-
-              <div id="f1_container">
-                <div id="f1_card" className="shadow">
-                  <div className="front face">
-                      <img className="card-img-top productimg" src="https://brickshow.com/wp-content/uploads/2017/10/10256_alt1.jpg"/>
-                  </div>
-                  <div className="back face center">
-                    <p>{tempProps.color}</p>
-
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Link>
-        </div>
-        <div className="col">
-          <div className="card">
-
-            <div id="f1_container">
-              <div id="f1_card" className="shadow">
-                <div className="front face">
-                  <img className="card-img-top productimg" src="https://brickshow.com/wp-content/uploads/2017/10/10256_alt1.jpg"/>
-                </div>
-                <div className="back face center">
-                  <p>This is nice for exposing more information about an image.</p>
-                  <p>Any content can go here.</p>
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col">
-          <div className="card">
-
-            <div id="f1_container">
-              <div id="f1_card" className="shadow">
-                <div className="front face">
-                  <img className="card-img-top productimg" src="https://brickshow.com/wp-content/uploads/2017/10/10256_alt1.jpg"/>
-                </div>
-                <div className="back face center">
-                  <p>This is nice for exposing more information about an image.</p>
-                  <p>Any content can go here.</p>
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-        <div className="col">
-          <div className="card">
-
-            <div id="f1_container">
-              <div id="f1_card" className="shadow">
-                <div className="front face">
-                  <img className="card-img-top productimg" src="https://brickshow.com/wp-content/uploads/2017/10/10256_alt1.jpg"/>
-                </div>
-                <div className="back face center">
-                  <p>This is nice for exposing more information about an image.</p>
-                  <p>Any content can go here.</p>
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-
-      </div>
-    </div>
+  <div className="container">
+    { tempProps.map(listing => <Listing listing={listing} key={listing.id}/>) }
   </div>
+
 );
+
+
 
 export default Listings;
