f73e0a2221eff77de6a66ea1e0ff94aa97938c66
Grant Spilsbury <grantspilsbury@gmail.com>
2018-01-17 03:35:33 +1100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || 'AKIAIYACVUQX5VLNLLQQ';
-module.exports.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || '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';
-module.exports.S3_BUCKET = process.env.S3_BUCKET || 'legotestimages';
-module.exports.SENDGRID_USER = process.env.SENDGRID_USER || 'grantspilsbury@gmail.com';
-module.exports.SENDGRID_PASSWORD = process.env.SENDGRID_PASSWORD || 'Tinley123';
-module.exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/legoTrader'
