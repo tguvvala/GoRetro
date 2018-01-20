ed0d4a3cafb41abcad5bb9338cb194f2e22986ec
Tejaswi <tejaswi.reddy6@gmail.com>
2018-01-19 19:55:18 -0800

    added Facebook Authentication

--- server/index.js
+++ server/index.js
@@ -3,14 +3,86 @@ const express = require('express');
 const db = require('../database/index');
 const mailer = require('../mailer/mailer');
 const aws = require('aws-sdk');
-
+var passport = require('passport');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+var Strategy = require('passport-facebook').Strategy;
+const User = db.User;
 const port = process.env.PORT || 8080;
+
+const url = 'http://localhost:8080';
+
+passport.use(new Strategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: url + '/login/facebook/return',
+  passReqToCallback: true
+},
+function(req, accessToken, refreshToken, profile, done) {
+  db.updateOrCreateUser({ fbId: profile.id, displayName: profile.displayName, sessionID: req.sessionID }, function (err, user) {
+    return done(err, user);
+  });
+}
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    if (err) {
+      done(err, null);
+    } else {
+      done(err, user);
+    }
+  });
+});
+
 
 const app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.use(require('cookie-parser')());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({
+  secret: 'b12gdh',
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login/facebook', passport.authenticate('facebook'));
+
+
+app.get('/login/facebook/return',
+  passport.authenticate('facebook', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/checkSession', (req, res) => {
+  User.findOne({ sessionID: req.sessionID }, (err, user) => {
+    if (user) {
+      res.send({isSignedIn: true, userId: user.fbId, username: user.displayName});
+    } else {
+      res.send(false);
+    }
+  });
+});
+
+app.get('/logOut', (req, res) => {
+  db.logout(req.sessionID, function() {
+    res.send(false);
+  });
+});
 
 const S3_BUCKET = process.env.S3_BUCKET;
 aws.config.region = 'us-east-2';
